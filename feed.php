2dfd7577ed714cbdf98a7886f9d79adb864e9449
“chiragawale” <chiragawale777@gmail.com>
2018-04-25 17:20:58 -0500

    Dynamic Query

--- feed.php
+++ feed.php
@@ -1,5 +1,5 @@
 <?php
-   
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password,$database);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 } 
 
 
@@ -22,39 +22,58 @@ if ($conn->connect_error) {
 
 
       // if(is_null($zip) or $zip == 0){
-      // $sql = "SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry from apartment";
+      //$sql = "SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry from apartment";
       // }else{
       //   $sql = "SELECT heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry from apartment where zip ='$zip'";
       // }
 
-      $zip = $_GET['zip'];
-      $maxRange = $_GET['maxRange'];
-      $minRange = $_GET['minRange'];
+$zip = $_GET['zip'];
+$maxRange = $_GET['maxRange'];
+$minRange = $_GET['minRange'];
 
 
-      $result = $conn->query($sql);
 
-      if ($result->num_rows > 0) {
+//build query
+$query = "SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry,likes from apartment";
+
+if(is_numeric($zip) or is_numeric($minRange) or is_numeric($maxRange)){
+  $query .= " WHERE ";
+}
+
+
+if(is_numeric($zip))
+ $query .= " zip = $zip";
+
+if(is_numeric($minRange))
+ $query .= " AND ( price >= $minRange";
+
+if(is_numeric($maxRange))
+ $query .= " AND price <= $maxRange) ";
+
+
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
           // output data of each row
-          while($row = $result->fetch_assoc()) {
-          
-              echo '<div class="card-main">
-              <h2> '.$row["heading"].'</h2>
-              <h5>'.$row["street"].', '.$row["city"].', '.$row["state"].', '.$row["zip"].'</h5>
-               <img style="height:250px; width: 350px;" src = "images/bedroom-1.jpg">
-              <p><strong>Description</strong></p>
-              <div> Price:  '.$row["price"].'</div>
-              <div> Rating:  '.$row["rating"].'</div>
-              <div> No of Bed/Baths:  '.$row["noofbedroom"].'/'.$row["noofbaths"].'</div>
-              <div> Gender Looking for:  '.$row["gender"].'</div>
-              <div> Pets:  '.$row["pets"].'</div>
-              <div> Laundry:  '.$row["laundry"].'</div>
-              <p></p>
-               </div>';
+  while($row = $result->fetch_assoc()) {
 
-        }
-      } else {
-          echo "0 results";
-      }
-      $conn->close();
+    echo '<div class="card-main">
+    <h2> '.$row["heading"].'</h2>
+    <h5>'.$row["street"].', '.$row["city"].', '.$row["state"].', '.$row["zip"].'</h5>
+    <img style="height:250px; width: 350px;" src = "images/bedroom-1.jpg">
+    <p><strong>Description</strong></p>
+    <div> Price:  '.$row["price"].'</div>
+    <div> Rating:  '.$row["rating"].'</div>
+    <div> No of Bed/Baths:  '.$row["noofbedroom"].'/'.$row["noofbaths"].'</div>
+    <div> Gender Looking for:  '.$row["gender"].'</div>
+    <div> Pets:  '.$row["pets"].'</div>
+    <div> Laundry:  '.$row["laundry"].'</div>
+    <p></p>
+    </div>';
+
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
