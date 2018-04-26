1aaa7077eb2576b85c2d38058b3d64dd84302c4d
smitshrestha101 <smitshrestha101@gmail.com>
2018-04-26 17:23:25 -0500

    dashboard db more

--- dashboard_db.php
+++ dashboard_db.php
@@ -1,9 +1,20 @@
+
+
 <?php
-	include 'db_connect.php';
+	include 'db-connect.php';
 
-	$uname=$_SESSION["username"]
+	$uname="smitshrestha101";
 
-	$stmt = $conn->prepare("SELECT street, city, state , zip from apartment WHERE AID IN (select AID from apply_apt where TID=0 and UID=)");
-	$stmt->bind_param("sssissss",$fname,$lname,$email,$phone,$address,$dob,$username,$password);
+$sql = "SELECT heading, street, city, state , zip, price, photo  from apartment WHERE AID IN (select AID from apply_apt where TID=0 and username='smitshrestha101')";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+       echo <img style="height:25px; width: 35px;" src = "'.$row["photo"].'">
+       $row['street'];
+    }  
+
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$conn->close();
 
 ?>
