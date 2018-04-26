9e1bb8930ef0c6dceba75709bd3a50c43bcb1f90
smitshrestha101 <smitshrestha101@gmail.com>
2018-04-26 00:19:07 -0500

    better UI for signup

--- signup.php
+++ signup.php
@@ -10,33 +10,33 @@
 <h1>Create Your Account</h1>
 
 <!-- One "tab" for each step in the form: -->
-<div class="tab">Name:
-  <p><input placeholder="First name..." oninput="this.className = ''"></p>
-  <p><input placeholder="Last name..." oninput="this.className = ''"></p>
+<div class="tab">Name
+  <p><input placeholder="First name" oninput="this.className = ''"></p>
+  <p><input placeholder="Last name" oninput="this.className = ''"></p>
 </div>
 
-<div class="tab">Contact Info:
-  <p><input placeholder="E-mail..." oninput="this.className = ''"></p>
-  <p><input placeholder="Phone..." oninput="this.className = ''"></p>
+<div class="tab">Contact Info
+  <p><input placeholder="E-mail" oninput="this.className = ''"></p>
+  <p><input placeholder="Phone" oninput="this.className = ''"></p>
 </div>
 
 <div class="tab">Address
-  <p><input placeholder="Street..." oninput="this.className = ''"></p>
-  <p><input placeholder="Apt #..." oninput="this.className = ''"></p>
-  <p><input placeholder="City..." oninput="this.className = ''"></p>
-  <p><input placeholder="State..." oninput="this.className = ''"></p>
-  <p><input placeholder="Zip..." oninput="this.className = ''"></p>
+  <p><input placeholder="Street" oninput="this.className = ''"></p>
+  <p><input placeholder="Apt#" oninput="this.className = ''"></p>
+  <p><input placeholder="City" oninput="this.className = ''"></p>
+  <p><input placeholder="State" oninput="this.className = ''"></p>
+  <p><input placeholder="Zip" oninput="this.className = ''"></p>
 </div>
 
-<div class="tab">Birthday:
+<div class="tab">Date of Birth
   <p><input placeholder="dd" oninput="this.className = ''"></p>
   <p><input placeholder="mm" oninput="this.className = ''"></p>
   <p><input placeholder="yyyy" oninput="this.className = ''"></p>
 </div>
 
-<div class="tab">Login Info:
-  <p><input placeholder="Username..." oninput="this.className = ''"></p>
-  <p><input placeholder="Password..." oninput="this.className = ''"></p>
+<div class="tab">Login Info
+  <p><input placeholder="Username" oninput="this.className = ''"></p>
+  <p><input placeholder="Password" oninput="this.className = ''"></p>
 </div>
 
 <div style="overflow:auto;">
