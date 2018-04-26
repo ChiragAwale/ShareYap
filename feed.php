14621bc26befa9c9119b37ddbf057156ebd7daa5
chiragawale <chiragawale777@gmail.com>
2018-04-26 00:18:36 -0500

    Dynamic Changes to page according to filter

--- feed.php
+++ feed.php
@@ -8,22 +8,34 @@ $code = $_GET['code'];
   $code = -1;
 }
 
-switch ($code) {
-    case "zip":
 
+if (empty($_GET['zip']) AND $code =='zip') {
+    $code = -1;
+}
+
+
+//PREPARES MYSQL QUERY ACCORDING TO USER ACTIONS
+
+switch ($code) {
+    case "maxRange":
+        $maxRange = $_GET['maxRange'];
+        // prepare and bind
+        $stmt = $conn->prepare("SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry,likes,dislike from apartment WHERE price  <= ?");
+        $stmt->bind_param("i", $maxRange);
+        break;
+    case "minRange":
+        $minRange = $_GET['minRange'];
+        // prepare and bind
+        $stmt = $conn->prepare("SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry,likes,dislike from apartment WHERE price  >= ?");
+        $stmt->bind_param("i", $minRange);
+        break;
+    case "zip":
         $zip = $_GET['zip'];
         // prepare and bind
         $stmt = $conn->prepare("SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry,likes,dislike from apartment WHERE zip = ?");
         $stmt->bind_param("i", $zip);
-        break;
-    case "maxRange":
-        $maxRange = $_GET['maxRange'];
-        break;
-    case "minRange":
-        $minRange = $_GET['minRange'];
-        break;
+        break;    
     default:
-
         $stmt = $conn->prepare("SELECT  heading, street, city,state,zip,price,rating,noofbedroom,noofbaths,gender,pets,laundry,likes,dislike from apartment");
 }
 
