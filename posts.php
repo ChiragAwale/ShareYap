9432b906341c6b132c01b623f2359ec739ed3d5d
chiragawale <chiragawale777@gmail.com>
2018-04-30 03:32:37 -0500

    Fix after pressing Lieks buttons

--- posts.php
+++ posts.php
@@ -35,11 +35,26 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   
     while($row = $result->fetch_assoc()) {
-       echo '
+       $aid = $row["aid"];
+    $sql_sl = 'SELECT app_status from apply_apt where TID=0 and username="'.$username.'" and aid = '. $aid;
+    $result_sl= $conn->query($sql_sl);
 
+    if ($result_sl->num_rows > 0) {
+    
+        echo '
+    <button  type = "button"> Applied </button>
+    <button  onclick = "clickFunction(1,'.$row["aid"].','.$row["likes"].')" value = "'.$row["likes"].'" type = "button"> Like ('.$row["likes"].')</button> 
+    <button onclick = "clickFunction(2,'.$row["aid"].','.$row["dislike"].')" value = "'.$row["dislike"].'" type = "button"> Dislike ('.$row["dislike"].')</button>
+    <button  onclick = "clickFunction(0,'.$row["aid"].','.$row["report"].')" value = "'.$row["report"].'" type = "button"> Report ('.$row["report"].')</button>'; 
+
+
+    }else{
+       echo '
+    <button onclick = "applyFunction('.$row["aid"].')"  type = "button"> Apply </button>
     <button  onclick = "clickFunction(1,'.$row["aid"].','.$row["likes"].')" value = "'.$row["likes"].'" type = "button"> Like ('.$row["likes"].')</button> 
     <button onclick = "clickFunction(2,'.$row["aid"].','.$row["dislike"].')" value = "'.$row["dislike"].'" type = "button"> Dislike ('.$row["dislike"].')</button>
     <button  onclick = "clickFunction(0,'.$row["aid"].','.$row["report"].')" value = "'.$row["report"].'" type = "button"> Report ('.$row["report"].')</button>';  
+    } 
     }  
 
 }
